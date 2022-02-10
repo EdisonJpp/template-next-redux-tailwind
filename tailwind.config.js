70ae8f19399c601a74b0bf13fbb992b2793c8332
Edison Padilla <edisojp@gmail.com>
2022-02-10 12:56:48 -0400

    feat: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      primary: "#003579",
+      primaryDark: "#031D4B",
+      primaryOutline: "#9FD0FD",
+
+      secondary: "#83A8C9",
+      secondaryOutline: "#F0F7FD",
+
+      danger: "#EE2A24",
+      dangerOutline: "#FFF2F4",
+
+      success: "#2ECC71",
+      successOutline: "#F6FFED",
+
+      normal: "#5D6267",
+    },
   },
   plugins: [],
-}
+};
