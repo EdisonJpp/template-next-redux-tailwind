8c9c7e10655a835d891dc812bd0b19a3d14d9d7b
Edison Padilla <edisojp@gmail.com>
2022-02-10 10:04:15 -0400

    feat: absolutes import and example

---
+++ components/user/Example.jsx
@@ -0,0 +1,10 @@
+const Example = () => {
+  return (
+    <div>
+      Example Example Example Example Example Example Example Example Example
+      Example Example Example Example Example
+    </div>
+  );
+};
+
+export default Example;
